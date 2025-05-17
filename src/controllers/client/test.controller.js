837a5f426305e8c126eee7ec16dac3d250c89e9c
nomenoparty <khronal777@gmail.com>
2025-05-17 15:35:37 +0700

    feat(test): create Test. Wwill doing edit, delete, do-test next

--- src/controllers/client/test.controller.js
+++ src/controllers/client/test.controller.js
@@ -1,3 +1,6 @@
+const Test = require("../../models/test.model");
+const Question = require("../../models/question.model");
+const Answer = require("../../models/answer.model");
 
 // [GET] /do-test
 module.exports.index = async (req, res) => {
@@ -19,21 +22,56 @@ module.exports.create = async (req, res) => {
 
 // [POST] /do-test/create
 module.exports.createPost = async (req, res) => {
-  const { title, timeLimit, questions } = req.body;
+  try {
+    const { title, timeLimit, questions } = req.body;
 
-  console.log("Title:", title);
-  console.log("Time Limit:", timeLimit);
+    const userID = req.tokenUser;
 
-  questions.forEach((question) => {
-    const title = question.title;
-    console.log("Question Title:", title);
+    const newTest = {
+      title: title,
+      time: timeLimit,
+      userId: userID
+    };
+
+    // Save the test to the database
+    const test = await Test.create(newTest);
+
+    await Promise.all(questions.map(async (question) => {
+      const title = question.title;
+
+      // Create the question object
+      const newQuestion = {
+        title: title,
+        testId: test._id
+      };
+
+      // Save the question to the database
+      const newQuestionCreated = await Question.create(newQuestion);
+
+      const answerOptions = question.answers;
+
+      await Promise.all(answerOptions.map(async (option) => {
+        const text = option.text;
+        const isCorrect = option.isCorrect;
+
+        const newAnswer = {
+          answer: text,
+          isCorrect: isCorrect,
+          questionId: newQuestionCreated._id
+        };
+
+        // Save the answer to the database
+        await Answer.create(newAnswer);
+      }));
+    }));
     
-    const answerOptions = question.answers;
-    answerOptions.forEach((option) => {
-      const text = option.text;
-      const isCorrect = option.isCorrect;
-
-      console.log("Answer Option:", text, "Is Correct:", isCorrect);
-    });
-  });
+    return res.json({
+      code: 200,
+      message: "Test created successfully",
+    })
+  } catch (error) {
+    console.error("Error creating test:", error);
+    req.flash("error", "An error occurred while creating the test");
+    res.redirect("/do-test/create");
+  }
 }
--- src/public/client/js/do-test.create.js
+++ src/public/client/js/do-test.create.js
@@ -233,6 +233,9 @@ document.addEventListener('DOMContentLoaded', function () {
       .then((response) => response.json())
       .then((data) => {
         console.log('Server Response:', data);
+        if (data.code == 200){
+          window.location.href = '/';
+        }
       })
       .catch((error) => {
         console.error('Error:', error);
